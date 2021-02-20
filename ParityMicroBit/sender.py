f6a403bdbf496aebfb5b5bf0dd828682d55f2263
Tomás de Camino Beck, Ph.D <tomas.decamino@gmail.com>
2021-02-20 10:36:07 -0600

    Update sender.py

--- ParityMicroBit/sender.py
+++ ParityMicroBit/sender.py
@@ -7,7 +7,25 @@ import radio
 
 radio.on()
  ##### Funtions. ######
-def sendBits(m):
+
+#count the number of ones in the string
+def countBit(m):
+    count = 0
+    for bit in m:
+        if bit == '1':
+            count +=1
+    return count
+
+#makes number of ones even, add '0' or '1' at the end   
+def parity(m): 
+    if countBit(m)&1 == 0:
+       return m + '0'
+    else:
+       return m + '1'
+
+
+#sends each 'bit' one by one. If button b pressed adds error (a '1')
+def sendBits(m): 
     for bit in message:
         if button_b.is_pressed():
             radio.send('1')
@@ -15,18 +33,21 @@ def sendBits(m):
             radio.send(bit)
         sleep(200)
     
+    
 ##### Main Code. ######
 
 while True:
     # Button A sends  message.
-    # You can send any message
-    message = '101011'
+    # you can send any sequence of 1 and ceros (binary number)
+    message = parity('10101') #adds parity bit at the end
     display.clear()
+    # send message when button a pressed
     if button_a.is_pressed():
         sendBits(message)
         display.scroll(message,delay=70)
 
-     # Sends again if error is detected
+     # Receives a message if receiver detects error
+     # then it sends the message again
     incoming = radio.receive()
     if incoming:
        display.show(Image.SAD)
